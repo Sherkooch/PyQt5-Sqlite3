d4ca323c2e196c1b0b03887c29bd4126e06d038a
Sherkooch <60571497+Sherkooch@users.noreply.github.com>
2021-07-10 19:29:14 +0430

    Update Prof.py

--- Prof.py
+++ Prof.py
@@ -15,7 +15,7 @@ class Ui_ProfWindow(object):
     
     def GET_DATA(self):
         self.window = QtWidgets.QMainWindow()
-        conn = sql.connect("D:\Me\Comp\PYTHON\Project For Poors\Kosar Yaghoobzadeh\INTERFACE\my_db.db")
+        conn = sql.connect("DataBase Adress in your PC")
         c = conn.cursor()
         
         self.PRID_input = self.InputNumber.text()
